04f4b39f002c8daf189df1c77ef3c469f02ae70b
Gerry Agbobada <gagbobada+git@gmail.com>
2018-05-27 19:34:53 +0200

    Initial commit : put all keywords in emacs

---
+++ mtc-mode.el
@@ -0,0 +1,145 @@
+;;; mtc-mode.el --- simple major mode for editing MTC files
+-*- coding: utf-8; lexical-binding: t; -*-
+
+;; Copyright © 2018, by Gerry Agbobada
+
+;; Author: Gerry Agbobada ( gagbobada+git@gmail.com )
+;; Version: 0.0.1
+;; Created: 27 May 2018
+;; Keywords: languages
+;; Homepage: http://cemef.mines-paristech.fr
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; You can redistribute this program and/or modify it under the terms of the MIT License
+
+;;; Commentary:
+
+;; Major mode for editing mtc files, scripts used by Cimlib
+
+;; The mode should load automatically on .mtc extension
+
+;;; Code:
+
+;; create the list for font-lock
+;; each category of keyword is given a particular face
+(setq mtc-font-lock-keywords
+      (let* (
+             (x-mtc-assign '(
+                             ;; Usual assignments
+                             "Type=" "Data="  "Entite=" "Dimension="
+                             "Degres=" "Min=" "Max=" "Valeurs=" "Priorite="
+                             "OperationsP1=" "Origine=" "Axes=" "NbChampsP1="
+                             "NbChamps=" "Numero=" "Initialise=" "Operateur="
+                             "Boucle=" "Fonction=" "NomCompteur="
+                             "NbChampSolution=" "NbChampParametre=" "Champ:"
+                             "ChampCondition=" "ObjetChamp=" "Operation="
+                             "Operations=" "Affichage=" "EcrirePartition="
+                             "SortieParElement="
+                             ;; Data related assign
+                             "NomFrequence=" "NomFichier=" "NomFichier:"
+                             "TypeFichier="
+                             ;; Mesh related assign
+                             "M:" "Partitionnement=" "Scripts=" "Remaillage="
+                             "Decale=" "Repartitionne="
+                             ;; Adaptation related assign
+                             "RAdaptation=" "Old=" "New=" "HAdaptation="
+                             "Effectue=" "Taille=" "Metrique=" "Qualite="
+                             "QualiteMin=" "Transport=" "Brique=" "TailleMax="
+                             "CreationNoeuds=" "DestructionNoeuds="
+                             ;; Fitz related assign
+                             "Fitz=" "TamponMetrique=" "LevelSets="
+                             "LevelSetsMultiple=" "PhaseModes="
+                             "PointPrecision=" "VolumePrecision="
+                             "QualityPrecision=" "YieldQuality="
+                             "PhasePrecision=" "IterationsMax="
+                             "NoeudsBloques=" "ElementsBloques=" "Cracks="
+                             "CracksMultiples=" "FiltresCracks="
+                             ;; Solveur related assign
+                             "Precision=" "IterMax=" "Resolution="
+                             "Preconditionneur=" "Methode=" "Implicite="
+                             "Galerkin=" "Thermique=" "PointFixe="
+                             "EpsRegularisation="
+                             ;; Geometre related assign
+                             "Forme=" "Centre=" "NormaleSortante=" "Normale="
+                             "Repere=" "Rayon="
+                             ))
+             (x-mtc-appel '(
+                            "Target=" "ChampParametre=" "ChampSolution="
+                            "ChampParametres=" "Geometre=" "Repere=" "Champ="
+                            "Appartient=" "NomTemps=" "NomPasDeTemps="
+                            "NomTempsFin="
+                            ))
+             (x-mtc-decla '( "Nom=" ))
+             (x-lanceur '( "Lanceur=" ))
+             (x-mtc-type '(
+                           ;; VChamp types
+                           "P1_Vecteur_Par" "P1_Scalaire_Par" "P1_Tenseur_Par"
+                           "P1_Tenseur_Sym_Par" "P0_Vecteur_Par"
+                           "P0_Scalaire_Par" "P0_Tenseur_Par"
+                           "P0_Tenseur_Sym_Par" "P0C_Scalaire_Par"
+                           "P0C_Vecteur_Par"  "P0C_Tenseur_Par"
+                           ;; Declaration of VChamp values
+                           "ValeurItem"
+                           ;; Declaration of CL or Geometre types
+                           "ModeleCLSurGeometres" "CLDirichlet"
+                           "GeometreAnalytique" "GeometreChamp" "Boule"
+                           "DemiPlan" "Brique"
+                           ;; I/O types
+                           "SortieVtu" "SortieVtuB"
+                           "SortieVtk" "SortieVtf" "SortieMtc" "SortieXdmf"
+                           "EntreeVtu" "EntreeVtuB"
+                           ))
+             (x-mtc-modele '(
+                             ;; Types for Modele=
+                             "ModeleParticulaire" "ModeleFonctionnel"
+                             "ModeleDeChamps" "ModeleElementsFinis"
+                             "ModeleMaillage" "ModeleConstant"
+                             "ModeleDeModeles" "ModeleSortie"
+                             "ModeleCLSurChamps" "ModeleArithmetique"
+                             "ModeleConditionnel" "ModeleDeGeometres"
+                             "ModeleDeMouvements" "ModeleAnalytique"
+                             "ModeleComportement" "ModelePostProcess"
+                             "ModeleIncremental" "ModeleTemporel"
+                             "ModeleFinSurTemps" "ModeleReprise"
+                             ))
+             (x-mtc-modele-arg '(
+                                 ;; Named arguments for Modele
+                                 "SimplexSolveurFonctionnel=" "ItemSolveur="
+                                 "SolveurLocal=" "Solveur=" "Dependance="
+                                 "DependanceModifiable=" "DependanceAEcrire="
+                                 "DependanceALire=" "nextgroup=mtcData" "skipwhite"
+                                 "Maillage=" "nextgroup=mtcData" "skipwhite"
+                                 "Modele=" "ModeleIncrement=" "ModeleTerminaison="
+                                 "ModeleApresFin=" "ModeleAvantDebut="
+                                 "ModeleTrue=" "ModeleFalse=" "ModeleCL="
+                                 "ModeleLoi=" "ConditionLimite="
+                                 ))
+             ;; generate regex string for each category
+             (x-mtc-assign-regexp (regex-opt x-mtc-assign 'words))
+             (x-mtc-appel-regexp (regex-opt x-mtc-appel 'words))
+             (x-mtc-decla-regexp (regex-opt x-mtc-decla 'words))
+             (x-lanceur-regexp (regex-opt x-lanceur 'words))
+             (x-mtc-type-regexp (regex-opt x-mtc-type 'words))
+             (x-mtc-modele-regexp (regex-opt x-mtc-modele 'words))
+             (x-mtc-modele-arg-regexp (regex-opt x-mtc-modele-arg 'words)))
+        (
+         (,x-lanceur-regexp . font-lock-constant-face)
+         (,x-mtc-decla-regexp . font-lock-builtin-face)
+         (,x-mtc-assign-regexp . font-lock-type-face)
+         (,x-mtc-type-regexp . font-lock-function-name-face)
+         (,x-mtc-modele-regexp . font-lock-function-name-face)
+         (,x-mtc-modele-arg-regexp . font-lock-builtin-face)
+         (,x-mtc-appel-regexp . font-lock-builtin-face)
+    )))
+
+(define-derived-mode mtc-mode c-mode "mtc mode"
+                     "Major mode for editing mtc scripts (Cemef's Cimlib interface)..."
+                     ;; code for syntax highlighting
+                     (setq font-lock-defaults '((mtc-font-lock-keywords))))
+
+(provide 'mtc-mode)
+
+;;; mtc-mode.el ends here
